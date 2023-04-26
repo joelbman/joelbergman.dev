1b81a6ca9f3e857d373fcc1a51a7f580dbc12f3c
Joel Bergman <joel.h.bergman@gmail.com>
2023-04-26 16:09:27 +0300

    Add meta tags & favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="img/favicon.png" />
+        <meta
+          name="description"
+          content="Personal portfolio page of Joel Bergman, a freelance web developer."
+        />
+        <meta name="keywords" content="Joel, Bergman, developer, web, software, freelancer" />
+        <meta name="author" content="Joel Bergman" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body>
         <Main />
         <NextScript />
