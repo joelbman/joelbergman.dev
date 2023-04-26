91de71a9795bf09b39833b9acb159d49e576a5ec
Joel Bergman <joel.h.bergman@gmail.com>
2023-04-26 18:13:12 +0300

    Move meta tags to app instead of document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <link rel="shortcut icon" href="img/favicon.png" />
+      <meta
+        name="description"
+        content="Personal portfolio page of Joel Bergman, a freelance web developer."
+      />
+      <meta name="keywords" content="Joel, Bergman, developer, web, software, freelancer" />
+      <meta name="author" content="Joel Bergman" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
-}
+  </>
+);
+
+export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="shortcut icon" href="img/favicon.png" />
-        <meta
-          name="description"
-          content="Personal portfolio page of Joel Bergman, a freelance web developer."
-        />
-        <meta name="keywords" content="Joel, Bergman, developer, web, software, freelancer" />
-        <meta name="author" content="Joel Bergman" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+const Document = () => (
+  <Html lang="en">
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
